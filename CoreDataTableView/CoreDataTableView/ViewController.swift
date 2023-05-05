560bdb292978e6eaa563661fc6981276492ffb10
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-05 17:00:55 +0300

    Add CoreData and Tasks

--- CoreDataTableView/CoreDataTableView/ViewController.swift
+++ CoreDataTableView/CoreDataTableView/ViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     // MARK: - IBOutlets
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
     
+    // MARK: - Private Properties
+    
+    var tasks = [Tasks]()
+    
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +28,28 @@ class ViewController: UIViewController {
     
     // MARK: - IBActions
     
-    @IBAction func addTaskAction(_ sender: UIBarButtonItem) {
+    @IBAction private func addTaskAction(_ sender: UIBarButtonItem) {
     }
     
-    @IBAction func removeTasksAction(_ sender: UIBarButtonItem) {
+    @IBAction private func removeTasksAction(_ sender: UIBarButtonItem) {
     }
     
 
 }
 
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row].title
+        
+        return cell
+    }
+}

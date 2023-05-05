3b9438467124deb1296ad23fa0f9c358b83884f5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-05 16:49:35 +0300

    Add outlets

--- CoreDataTableView/CoreDataTableView/ViewController.swift
+++ CoreDataTableView/CoreDataTableView/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - IBActions
+    
+    @IBAction func addTaskAction(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func removeTasksAction(_ sender: UIBarButtonItem) {
+    }
+    
 
 }
 
